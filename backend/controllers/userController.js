4032b3abeed14df6d58dbfcba7f50787a57178c0
Jose Lara-Urbina <joselaraurb@gmail.com>
2024-03-12 17:22:05 -0400

    Creating an authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,6 +70,18 @@ exports.loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+exports.getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 //Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
 
+const { protect } = require('../middleware/authMiddleware');
+
 router.post('/', controller.registerUser);
 
 router.post('/login', controller.loginUser);
+
+router.get('/me', protect, controller.getMe);
 
 module.exports = router;
