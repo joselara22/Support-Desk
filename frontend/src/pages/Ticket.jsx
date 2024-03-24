6297b2dfa285079036c6b058c5d1d1bd0351043e
Jose Lara-Urbina <joselaraurb@gmail.com>
2024-03-23 23:40:38 -0400

    Linking note creation to backend

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
-import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
+import {
+  getNotes,
+  createNote,
+  reset as notesReset,
+} from '../features/notes/noteSlice';
 import { useParams, useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
@@ -61,7 +65,7 @@ function Ticket() {
   const onNoteSubmit = (e) => {
     e.preventDefault();
 
-    console.log('Submit');
+    dispatch(createNote({ noteText, ticketId }));
     closeModal();
   };
 
